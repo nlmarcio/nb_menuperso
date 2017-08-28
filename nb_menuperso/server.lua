e79a9f730d92df1eba28791557a330aa1b2e208e
Charly <charly.strn@gmail.com>
2017-08-28 23:17:45 +0200

    Grade menu : Recruter/Virer/Promouvoir/Rétrograder

---
+++ nb_menuperso/server.lua
@@ -0,0 +1,121 @@
+ESX = nil
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+function getMaximumGrade(jobname)
+    local result = MySQL.Sync.fetchAll("SELECT * FROM job_grades WHERE job_name=@jobname  ORDER BY `grade` DESC ;", {
+        ['@jobname'] = jobname
+    })
+    if result[1] ~= nil then
+        return result[1].grade
+    end
+    return nil
+end
+
+
+
+RegisterServerEvent('esx:promouvoirplayer')
+AddEventHandler('esx:promouvoirplayer', function(target)
+
+	local _source = source
+
+	local sourceXPlayer = ESX.GetPlayerFromId(_source)
+	local targetXPlayer = ESX.GetPlayerFromId(target)
+	local maximumgrade = tonumber(getMaximumGrade(sourceXPlayer.job.name)) -1 
+
+	if(targetXPlayer.job.grade == maximumgrade)then
+		TriggerClientEvent('esx:showNotification', _source, "Vous devez demander une autorisation du ~r~Gouvernement~w~.")
+	else
+		if(sourceXPlayer.job.name == targetXPlayer.job.name)then
+
+			local grade = tonumber(targetXPlayer.job.grade) + 1 
+			local job = targetXPlayer.job.name
+
+			targetXPlayer.setJob(job, grade)
+
+			TriggerClientEvent('esx:showNotification', _source, "Vous avez ~g~promu "..targetXPlayer.name.."~w~.")
+			TriggerClientEvent('esx:showNotification', target,  "Vous avez été ~g~promu par".. sourceXPlayer.name.."~w~.")		
+
+		else
+			TriggerClientEvent('esx:showNotification', _source, "Vous n'avez pas ~r~l'autorisation~w~.")
+
+		end
+
+
+	end 
+		
+end)
+
+
+RegisterServerEvent('esx:destituerplayer')
+AddEventHandler('esx:destituerplayer', function(target)
+
+	local _source = source
+
+	local sourceXPlayer = ESX.GetPlayerFromId(_source)
+	local targetXPlayer = ESX.GetPlayerFromId(target)
+
+	if(targetXPlayer.job.grade == 0)then
+		TriggerClientEvent('esx:showNotification', _source, "Vous ne pouvez pas plus ~r~rétrograder~w~.")
+	else
+		if(sourceXPlayer.job.name == targetXPlayer.job.name)then
+
+			local grade = tonumber(targetXPlayer.job.grade) - 1 
+			local job = targetXPlayer.job.name
+
+			targetXPlayer.setJob(job, grade)
+
+			TriggerClientEvent('esx:showNotification', _source, "Vous avez ~r~rétrogradé "..targetXPlayer.name.."~w~.")
+			TriggerClientEvent('esx:showNotification', target,  "Vous avez été ~r~rétrogradé par".. sourceXPlayer.name.."~w~.")		
+
+		else
+			TriggerClientEvent('esx:showNotification', _source, "Vous n'avez pas ~r~l'autorisation~w~.")
+
+		end
+
+
+	end 
+		
+end)
+
+
+
+
+
+
+
+
+
+RegisterServerEvent('esx:recruterplayer')
+AddEventHandler('esx:recruterplayer', function(target, job, grade)
+
+	local _source = source
+
+	local sourceXPlayer = ESX.GetPlayerFromId(_source)
+	local targetXPlayer = ESX.GetPlayerFromId(target)
+	
+		targetXPlayer.setJob(job, grade)
+
+		TriggerClientEvent('esx:showNotification', _source, "Vous avez ~g~recruté "..targetXPlayer.name.."~w~.")
+		TriggerClientEvent('esx:showNotification', target,  "Vous avez été ~g~embauché par".. sourceXPlayer.name.."~w~.")		
+
+
+end)
+
+RegisterServerEvent('esx:virerplayer')
+AddEventHandler('esx:virerplayer', function(target)
+
+	local _source = source
+
+	local sourceXPlayer = ESX.GetPlayerFromId(_source)
+	local targetXPlayer = ESX.GetPlayerFromId(target)
+	local job = "unemployed"
+	local grade = "0"
+
+		targetXPlayer.setJob(job, grade)
+
+		TriggerClientEvent('esx:showNotification', _source, "Vous avez ~r~viré "..targetXPlayer.name.."~w~.")
+		TriggerClientEvent('esx:showNotification', target,  "Vous avez été ~g~viré par".. sourceXPlayer.name.."~w~.")		
+
+
+end)
